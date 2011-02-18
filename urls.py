93bbeace7e3c75c33b076cb4cc4c3f4bb82e2cb3
-gHoTi- <ghoti2007@gmail.com>
2011-02-17 21:57:14 -0500

    Deploying?!

--- urls.py
+++ urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': './static/'}),
+    #(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': './static/'}),
 
     (r'$^', views.index),
     (r'^live/$', views.live),
